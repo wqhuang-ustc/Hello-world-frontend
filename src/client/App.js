4c2fd90c63a7a2d5f708fe4bdb2603f6ff7959b5
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-03-10 18:38:03 +0100

    visualization of a sensor data

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 import Button from '@material-ui/core/Button';
+import LineCharts from './components/LineCharts.js';
 
 export default class App extends Component {
   state = { uptime: null };
@@ -24,6 +25,7 @@ export default class App extends Component {
       <div>
         {uptime ? <h1>{`Hello World! The system's uptime is ${uptime}`}</h1> : <h1>Loading.. please wait!</h1>}
         <Button size="small" variant="contained" color="primary" onClick={this.handleClick}>update data</Button>
+        <LineCharts />
         {/* <div><img src={ReactImage} alt="react" /></div> */}
       </div>
     );
---
+++ src/client/components/LineCharts.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import ReactChartkick, { LineChart } from 'react-chartkick';
+import Chart from 'chart.js';
+import Button from '@material-ui/core/Button';
+
+ReactChartkick.addAdapter(Chart);
+
+class LineCharts extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            height: '145px'
+        };
+    }
+
+    componentDidMount() {
+        this.initState();
+        // update frontend data every 30 minutes
+        this.interval = setInterval(() => this.updateData(), 1800000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    // Display all init data, hide button if data is less than 24 hours
+    initState = () => {
+        fetch('/api/updateData')
+          .then(res => res.json())
+          .then(res => this.setState({
+            all_active: 1,
+            recent_active: 0,
+            multiview: 1,
+            DataSensor1: res[0],
+            time: (new Date()).toISOString().split('.')[0].replace('T', '\xa0\xa0')
+          }));
+        console.log('Data init '.concat((new Date()).toISOString().split('.')[0].replace('T', ',')));
+    
+        fetch('/api/updateData')
+          .then(res => res.json())
+          .then((res) => {
+            if (res[0].length < 25) {
+              this.setState({
+                multiview: 0,
+              });
+            }
+          });
+      }  
+
+  // update data,  "all_active" save state of whether in the 24-hour view, 
+  updateData = () => {
+    fetch('/api/updateData')
+      .then(res => res.json())
+      .then((res) => {
+        if (this.state.all_active === 1) {
+          this.setState({
+            DataSensor1: res[0],
+            time: (new Date()).toISOString().split('.')[0].replace('T', '\xa0\xa0')
+          });
+        } else {
+          this.setState({
+            DataSensor1: res[0].slice(Math.max(res[0].length - 24, 1)),
+            time: (new Date()).toISOString().split('.')[0].replace('T', '\xa0\xa0')
+          });
+        }
+      });
+    console.log('Data updated '.concat((new Date()).toISOString().split('.')[0].replace('T', ',')));
+  }
+
+    handleClickerAll = () => {
+        fetch('/api/updateData')
+        .then(res => res.json())
+        .then(res => this.setState({
+          all_active: 1,
+          recent_active: 0,
+          DataSensor1: res[0],
+          time: (new Date()).toISOString().split('.')[0].replace('T', '\xa0\xa0')
+        }));
+    }
+
+    handleClickRecent = () => {
+        fetch('/api/updateData')
+        .then(res => res.json())
+        .then(res => this.setState({
+          all_active: 0,
+          recent_active: 1,
+          DataSensor1: res[0].slice(Math.max(res[0].length - 24, 1)),
+          time: (new Date()).toISOString().split('.')[0].replace('T', '\xa0\xa0')
+        }));
+      }
+
+    render(){
+        const {height,DataSensor1,time } = this.state;
+
+        return (
+            <div>
+                <div className="charts">
+                    <h1>Data visualization of a sensors</h1>
+                    <p className="update_time">Data updated at {time}</p>
+                    <LineChart
+                    data={DataSensor1}
+                    label="value"
+                    ytitle="Sensor 1"
+                    height={height}
+                    />
+                </div>
+
+                <div className={this.state.multiview ? '' : "hide"}>
+                    <span
+                        className= {this.state.all_active ? "button_active" : "button_inactive"}
+                        onClick={this.handleClickerAll}
+                    >
+                        <Button size="small" variant="contained" color="primary">All Data Retrieved</Button>
+                    </span>
+                    <span
+                        className= {this.state.recent_active ? "button_active" : "button_inactive"}
+                        onClick={this.handleClickRecent}
+                    >
+                        <Button size="small" variant="contained" color="primary">Recent 24 Hours</Button>
+                    </span>
+                </div>
+
+        </div>
+
+        );
+    }
+}
+
+export default LineCharts;
